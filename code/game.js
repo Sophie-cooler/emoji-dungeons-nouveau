2a818fb4d9486e1f4898b1d841aca5e0ce6fa796
Erik <162273639+IsPrism@users.noreply.github.com>
2024-07-19 16:34:29 -0600

    Update borders again

--- code/game.js
+++ code/game.js
@@ -64,6 +64,11 @@ function newCombatant(team, id, level) {
 function battleFlow() {
     setTimeout(function() {
         removeBattleText();
+        
+        allPlayers.forEach(combatant => if(combatant.hasMoved == 1){
+            combatant.style.borderColor = "red";
+        })
+
         if (allPlayers.every(combatant => combatant.hasMoved == 1)){
             allEnemies.forEach(combatant => combatant.performActionAsEnemy());
             allPlayers.forEach(combatant => combatant.hasMoved = 0);
